d07180c31136024da1cff43e5728b60ade563f97
mathe <mathe@DESKTOP-06LM2TJ>
2024-08-24 13:43:02 -0300

    Adicionar arquivos de projeto.

---
+++ Converter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula3_cs_ex6
+{
+    public class Converter
+    {
+        public bool Validação(string valorS) 
+        {
+            if(valorS.Length > 6) 
+            {
+                Console.WriteLine("Erro 0, valor invalido de caracteres");
+                return false;
+
+            }
+            foreach (char c in valorS)
+            {
+                if (c != '0' && c != '1')
+                {
+                    Console.WriteLine("Erro 1, valor não é binario");
+                    return false;
+                }
+            }
+            return true;        
+        }
+        public int logica(string valorS) 
+        {
+            int valordecimal = Convert.ToInt32(valorS, 2);
+            return valordecimal;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula3_cs_ex6
+{
+    internal class Program
+    {
+        bool valor = false;
+        string valorstring;
+        int valordecimal;
+        static void Main(string[] args)
+        {
+            Program start = new Program();
+        }
+        public Program() 
+        {
+            Converter conversor = new Converter();
+            while(valor != true)
+            {
+                Console.Write("Digite um valor em binario com até 6 caracteres: ");
+                valorstring = Console.ReadLine();
+                valor = conversor.Validação(valorstring);
+            }
+            valordecimal = conversor.logica(valorstring);
+            Console.WriteLine("O valor decimal foi: {0}", valordecimal);
+            Console.ReadKey();
+        }
+        
+    
+    }
+}
